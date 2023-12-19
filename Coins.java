b7901dbafe8f0b7fa8528290299c401b81c0e50f
lkarsenty <154309399+lkarsenty@users.noreply.github.com>
2023-12-19 11:13:00 +0300

    Update Coins.java

--- Coins.java
+++ Coins.java
@@ -4,6 +4,9 @@
  */
 public class Coins {
 	public static void main(String[] args) {
-		// Put your code here
+		int sum = Integer.parseInt(args[0]);
+      		int quarters = sum/25;
+      		int cents = sum%25;
+      		System.out.println("Use " + quarters + " quarters and " + cents + " cents");
 	}
 }
